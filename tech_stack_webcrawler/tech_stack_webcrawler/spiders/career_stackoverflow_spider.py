0f0db806144186993940fb776735d2b7abb25364
SSDong <duanlang5@gmail.com>
2015-11-23 20:32:22 -0500

    get links and do http requests for them

--- tech_stack_webcrawler/tech_stack_webcrawler/spiders/career_stackoverflow_spider.py
+++ tech_stack_webcrawler/tech_stack_webcrawler/spiders/career_stackoverflow_spider.py
@@ -3,12 +3,18 @@ import scrapy
 class CareerStackoverflowSpider(scrapy.Spider):
     name = "careerstackoverflow"
     start_urls = [
-        "http://careers.stackoverflow.com/companies"
+        "http://careers.stackoverflow.com/companies?pg=4"
     ]
 
     def parse(self, response):
-        # print response.css(".list.companies").xpath("div").extract()
-        filename = response.url.split("/")[-2] + ".html"
-        with open(filename, "wb") as f:
-            for sel in response.css(".list.companies").xpath("div"):
-                f.write(sel.xpath("p/a[@class='title']/text()").extract()[0] + "\n")
+        # filename = response.url.split("/")[-2] + ".html"
+        # with open(filename, "wb") as f:
+        #     f.write(response.body)
+
+        for company_link in response.css(".list.companies").xpath("div/p/a[@class='title']/@href"):
+            if company_link:
+                company_link = response.urljoin(company_link.extract())
+                yield scrapy.Request(company_link, self.parse_company)
+
+    def parse_company(self, response):
+        print "This is a test"
