2d2b9627d73437590c621ab8c0d1fdcb83ca3539
SSDong <duanlang5@gmail.com>
2016-02-29 10:20:17 -0500

    updated xpath

--- tech_stack_webcrawler/tech_stack_webcrawler/spiders/career_stackoverflow_spider.py
+++ tech_stack_webcrawler/tech_stack_webcrawler/spiders/career_stackoverflow_spider.py
@@ -6,15 +6,19 @@ class CareerStackoverflowSpider(scrapy.Spider):
         "http://careers.stackoverflow.com/companies"
     ]
 
+    # Notice that the path to the targeted data might change in the future.
     def parse(self, response):
-        for company_link in response.css(".list.companies").xpath("div/p/a[@class='title']/@href"):
+        for company_link in response.css(".list.companies").xpath("div/div[@class='text']/p/a[@class='title']/@href"):
             if company_link:
                 company_link = response.urljoin(company_link.extract())
                 yield scrapy.Request(company_link, self.parse_company)
 
     def parse_company(self, response):
-        tech_stack = response.xpath("//div[@data-company-section='tech-stack']")
+        file = open("tech.json", "a")
+        tech_stack = response.xpath("//div[@data-company-section='tech-stack']/div[@class='tags']")
 
         if tech_stack:
-            for tech_tag in tech_stack.xpath("div[@class='tags']/span[@class='post-tag']/text()"):
-                print tech_tag.extract()
+            for tech_tag in tech_stack.xpath("a[contains(@class, 'post-tag')]/text()"):
+                file.write(tech_tag.extract() + "\n")
+
+        file.close()
